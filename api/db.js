3d404eb011a8ed5539828ed8971f8e9413a6e392
Jeremy J. Johnson <jeremyjohnson7@mail.weber.edu>
2017-04-09 02:51:08 -0600

    Connection string

--- api/db.js
+++ api/db.js
@@ -3,7 +3,7 @@ const connection_string =
         ? `${process.env.PENSHIFT_MONGODB_DB_URL}/${process.env.OPENSHIFT_APP_NAME}`
         : 'localhost:27017/cookbook';
 
-console.log(connection_string);
+console.log('* * *   C O N N E C T I O N   S T R I N G   * * *', connection_string);
 
 const mongojs = require('mongojs');
 module.exports = mongojs(connection_string, ['recipes', 'users']);
