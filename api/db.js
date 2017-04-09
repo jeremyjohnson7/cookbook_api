7f9bb7998ab38f8bf5af0f051e1ea833a84fbca8
Jeremy J. Johnson <jeremyjohnson7@mail.weber.edu>
2017-04-09 02:14:23 -0600

    Connection string

--- api/db.js
+++ api/db.js
@@ -1,7 +1,7 @@
 let connection_string = 'localhost:27017/cookbook';
 // If OPENSHIFT env variables are present, use the available connection info:
 if (process.env.OPENSHIFT_MONGODB_DB_URL)
-    connection_string = `${OPENSHIFT_MONGODB_DB_URL}/${process.env.OPENSHIFT_APP_NAME}`;
+    connection_string = `${process.env.PENSHIFT_MONGODB_DB_URL}/${process.env.OPENSHIFT_APP_NAME}`;
 
 const mongojs = require('mongojs');
 module.exports = mongojs(connection_string, ['recipes', 'users']);
