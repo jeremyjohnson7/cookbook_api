e875ee5558d294fe9adab6431bbb1c683a90dcfb
Jeremy J. Johnson <jeremyjohnson7@mail.weber.edu>
2017-04-09 02:09:21 -0600

    Database credentials

--- api/db.js
+++ api/db.js
@@ -1,12 +1,7 @@
 let connection_string = 'localhost:27017/cookbook';
-// if OPENSHIFT env variables are present, use the available connection info:
-if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-    process.env.OPENSHIFT_MONGODB_DB_PORT + '/';
-    process.env.OPENSHIFT_APP_NAME;
-}
+// If OPENSHIFT env variables are present, use the available connection info:
+if (process.env.OPENSHIFT_MONGODB_DB_URL)
+    connection_string = `${OPENSHIFT_MONGODB_DB_URL}/${process.env.OPENSHIFT_APP_NAME}`;
 
 const mongojs = require('mongojs');
 module.exports = mongojs(connection_string, ['recipes', 'users']);
