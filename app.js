6572311092cc3fb9ba15df5857bb6b7bac23439b
Jeremy J. Johnson <jeremyjohnson7@mail.weber.edu>
2017-04-09 01:55:39 -0600

    Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ app.use(bodyParser.json());
 
 // Set headers
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://cookbook.netlify.com');
+    res.setHeader('Access-Control-Allow-Origin',
+        `http://${process.env.NODE_IP || 'localhost'}:${process.env.NODE_PORT || 3000}`);
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Cache-Control', 'no-cache, no-store');
