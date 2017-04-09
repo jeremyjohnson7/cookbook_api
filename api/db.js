72c47c24732d3a2b586e3a662f34d1bcbce9b32b
Jeremy J. Johnson <jeremyjohnson7@mail.weber.edu>
2017-04-09 01:38:30 -0600

    Fixed credentials

--- api/db.js
+++ api/db.js
@@ -1,11 +1,11 @@
-let connection_string = '127.0.0.1:27017/cookbook';
+let connection_string = 'localhost:27017/cookbook';
 // if OPENSHIFT env variables are present, use the available connection info:
 if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
     connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
     process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
     process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-    process.env.OPENSHIFT_MONGODB_DB_PORT + '/cookbook';
-    // process.env.OPENSHIFT_APP_NAME;
+    process.env.OPENSHIFT_MONGODB_DB_PORT + '/';
+    process.env.OPENSHIFT_APP_NAME;
 }
 
 const mongojs = require('mongojs');
